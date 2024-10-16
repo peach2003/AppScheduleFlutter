e4933412aaf32dea48cdee4caa83f4ca6f566e72
Đỗ Thanh Đào <dothanhdao37577@gmail.com>
2024-10-16 15:32:55 +0700

    commit Home Event

---
+++ app_schedule_flutter/lib/Screen/Detail_Event_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class DetailEvent extends StatefulWidget {
+  const DetailEvent({super.key});
+
+  @override
+  State<DetailEvent> createState() => _DetailEventState();
+}
+
+class _DetailEventState extends State<DetailEvent> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
